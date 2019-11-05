a46a67b03665d991d95ebef08e7c48dfe904339a
Dan McKinney <3dhdaniel@gmail.com>
2019-11-04 22:17:01 -0500

    lotta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { Routes, RouterModule  } from '@angular/router'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { RentalComponent } from './rental/rental.component';
-import { RentalModule } from './rental/rental.module'
+import { RentalModule } from './rental/rental.module';
 
 const routes: Routes = [
   {path: '', redirectTo: '/rentals', pathMatch: 'full'},
@@ -17,6 +17,7 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     HeaderComponent
+    
    
   ],
   imports: [
---
+++ src/app/common/map/map.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { MapComponent } from './map.component'
+import { AgmCoreModule } from '@agm/core';
+import { CommonModule } from '@angular/common';
+
+import { MapService } from './map.service'
+import { CamelizePipe } from 'ngx-pipes';
+
+@NgModule({
+  declarations: [
+    MapComponent
+  ],
+  exports: [
+    MapComponent
+  ],
+  imports: [
+    AgmCoreModule.forRoot({
+        apiKey: 'AIzaSyDW9tFSqG2mA0ym2NluRBVGZ6tPr8xbwRM'
+      }),
+      CommonModule
+  ],
+  providers: [
+      MapService,
+      CamelizePipe
+  ]
+})
+export class MapModule { }

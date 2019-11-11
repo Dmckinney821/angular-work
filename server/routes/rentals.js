fb767e38f0e7c6aa2507739308643ef730269485
Dan McKinney <3dhdaniel@gmail.com>
2019-11-10 20:01:31 -0500

    adds booking creation functionality and improvements

--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -11,19 +11,25 @@ router.get('/secret', UserContrl.authMiddleware, (req, res) => {
 
 
 router.get('', (req,res) => {
-  Rental.find({}, (err,foundRental) => {
-    res.json(foundRental);
-  });
+  Rental.find()
+    .select('-bookings')
+    .exec((err, foundRentals) => {
+      res.json(foundRentals)
+    })
 });
 
 router.get('/:id', (req, res) => {
     const rentalId = req.params.id;
-    Rental.findById(rentalId, (err, foundRental) => {
+    
+    Rental.findById(rentalId)
+      .populate('user', 'username -_id')
+      .populate('bookings', 'startAt endAt -_id')
+      .exec((err, foundRentals) => {
         if(err) {
-            res.status(422).send({errors: [{title: 'Rental Error!', detail: 'Could not find Rental'}]})
-        }
-        res.json(foundRental)
-    })
+          return res.status(422).send({errors: [{title: 'Rental Error!', detail: 'Could not find Rental'}]})
+          }
+          return res.json(foundRentals)
+      })
 })
 
 module.exports = router
